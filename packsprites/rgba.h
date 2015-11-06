fa798d24d451837d92dfc38003a469ec3499861a
mpersano <mpr@fzort.org>
2015-11-06 01:49:14 -0200

    why not

--- packsprites/rgba.h
+++ packsprites/rgba.h
@@ -1,15 +1,96 @@
 #pragma once
 
+template <template <typename> class Base, typename T>
+struct vec_ops
+{
+	template <typename U>
+	Base<T>& operator*=(U rv)
+	{
+		Base<T>& lv = static_cast<Base<T> &>(*this);
+		for (size_t i = 0; i < Base<T>::size; i++)
+			lv[i] *= rv;
+		return lv;
+	}
+
+	template <typename U>
+	Base<T>& operator+=(const Base<U>& rv)
+	{
+		Base<T>& lv = static_cast<Base<T> &>(*this);
+		for (size_t i = 0; i < Base<T>::size; i++)
+			lv[i] += rv[i];
+		return lv;
+	}
+
+	template <typename U>
+	Base<T>& operator-=(const Base<U>& rv)
+	{
+		Base<T>& lv = static_cast<Base<T> &>(*this);
+		for (size_t i = 0; i < Base<T>::size; i++)
+			lv[i] -= rv[i];
+		return lv;
+	}
+
+	template <typename U>
+	friend inline
+	Base<T> operator*(const Base<T>& lv, U rv)
+	{ return Base<T>(lv) *= rv; }
+
+	template <typename U>
+	friend inline
+	Base<T> operator*(U lv, const Base<T>& rv)
+	{ return Base<T>(rv) *= lv; }
+
+	template <typename U>
+	friend inline
+	Base<T> operator+(const Base<T>& lv, const Base<U>& rv)
+	{ return Base<T>(lv) += rv; }
+
+	template <typename U>
+	friend inline
+	Base<T> operator-(const Base<T>& lv, const Base<U>& rv)
+	{ return Base<T>(lv) -= rv; }
+};
+
 template <typename T>
-struct rgba
+struct rgb : vec_ops<rgb, T>
+{
+	rgb()
+	: r(0), g(0), b(0)
+	{ }
+
+	template <typename U>
+	rgb(const rgb<U>& rv)
+	: r(rv.r), g(rv.g), b(rv.b)
+	{ }
+
+	template <typename R, typename G, typename B>
+	rgb(R r, G g, B b)
+	: r(r), g(g), b(b)
+	{ }
+
+	const T& operator[](size_t i) const
+	{
+		return v[i];
+	}
+
+	T& operator[](size_t i)
+	{
+		return v[i];
+	}
+
+	static const size_t size = 3;
+
+	union {
+		struct { T r, g, b; };
+		T v[size];
+	};
+};
+
+template <typename T>
+struct rgba : vec_ops<rgba, T>
 {
 	rgba()
 	: r(0), g(0), b(0), a(0)
-	{ }
-
-	template <typename R, typename G, typename B, typename A>
-	rgba(R r, G g, B b, A a)
-	: r(r), g(g), b(b), a(a)
 	{ }
 
 	template <typename U>
@@ -17,58 +98,30 @@ struct rgba
 	: r(rv.r), g(rv.g), b(rv.b), a(rv.a)
 	{ }
 
-	template <typename U>
-	rgba<T>& operator*=(U rv)
+	template <typename R, typename G, typename B, typename A>
+	rgba(R r, G g, B b, A a)
+	: r(r), g(g), b(b), a(a)
+	{ }
+
+	template <typename U, typename A>
+	rgba(const rgb<U>& v, A a)
+	: r(v.r), g(v.g), b(v.b), a(a)
+	{ }
+
+	const T& operator[](size_t i) const
 	{
-		r *= rv;
-		g *= rv;
-		b *= rv;
-		a *= rv;
-		return *this;
+		return v[i];
 	}
 
-	template <typename U>
-	rgba<T>& operator+=(const rgba<U>& rv)
+	T& operator[](size_t i)
 	{
-		r += rv.r;
-		g += rv.g;
-		b += rv.b;
-		a += rv.a;
-		return *this;
+		return v[i];
 	}
 
-	template <typename U>
-	rgba<T>& operator-=(const rgba<U>& rv)
-	{
-		r -= rv.r;
-		g -= rv.g;
-		b -= rv.b;
-		a -= rv.a;
-		return *this;
-	}
+	static const size_t size = 4;
 
-	template <typename U>
-	friend inline
-	rgba<T> operator*(const rgba<T>& lv, U rv)
-	{ return rgba<T>(lv) *= rv; }
-
-	template <typename U>
-	friend inline
-	rgba<T> operator*(U lv, const rgba<T>& rv)
-	{ return rgba<T>(rv) *= lv; }
-
-	template <typename U>
-	friend inline
-	rgba<T> operator+(const rgba<T>& lv, const rgba<U>& rv)
-	{ return rgba<T>(lv) += rv; }
-
-	template <typename U>
-	friend inline
-	rgba<T> operator-(const rgba<T>& lv, const rgba<U>& rv)
-	{ return rgba<T>(lv) -= rv; }
-
-	T r, g, b, a;
+	union {
+		struct { T r, g, b, a; };
+		T v[size];
+	};
 };
-
-using rgbaf = rgba<float>;
-using rgbai = rgba<int>;
